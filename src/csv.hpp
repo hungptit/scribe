8fa32ccbae269b3c11562879274077ecbcf2f62a
Hung Dang <hdang@athenahealth.com>
2018-12-24 21:12:42 -0500

    Add a place holder for CSVPolicy.

---
+++ src/csv.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstring>
+
+namespace scribe {
+    struct CSVPolicy {
+        void operator()(const char *begin, const size_t len) {
+            fmt::print("TODO: Implement this method!");
+        }
+    };
+} // namespace scribe
